321598589c7cfe09a541732adc03c807464d9dab
Gary Madsen <gm@tradium.dk>
2021-05-19 13:55:14 +0200

    Add project files.

---
+++ Hangmantest/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Hangmantest
+{//https://stackoverflow.com/questions/35586708/hangman-console-application
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] guessed = new char[26];
+            char[] testword = "******".ToCharArray();
+            char[] word = "miller".ToCharArray();
+            char[] copy = word;
+
+            char guess;
+
+            int score = 0, index = 0;
+            Console.WriteLine(testword);
+
+            for (int i = 0; i < 10; i++)
+            {
+
+                Console.Write("Please enter a letter to guess: ");
+
+                guess = char.Parse(Console.ReadLine());
+                bool right = false;
+                for (int j = 0; j < copy.Length; j++)
+                {
+                    if (copy[j] == guess)
+                    {
+                        Console.WriteLine("Your guess is correct.");
+                        testword[j] = guess;
+                        guessed[index] = guess;
+                        index++;
+                        right = true;
+                    }
+                }
+
+                if (right != true)
+                {
+                    Console.WriteLine("Your guess is incorrect.");
+                    score++;
+                }
+                else
+                {
+                    right = false;
+                }
+
+                Console.WriteLine(testword);
+
+
+            }
+
+            Console.WriteLine("Your score is " + score);
+
+            Console.ReadLine(); ;
+        }
+    }
+}
